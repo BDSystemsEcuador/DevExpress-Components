d6aa032fa4d39582549f4e29be305c63a5b03274
BDSystems-Ecuador <byrondanipm@gmail.com>
2021-03-30 20:35:49 -0500

    proyectos devexpress

---
+++ DataForm/Prueba1/Prueba1/EmployeeInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace Prueba1
+{
+    public class EmployeeInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public Department Department { get; set; }
+    }
+    public enum Department { Sales, Support, Shipping, Engineering, HumanResources, Management, IT }
+}
---
+++ DataForm/Prueba1/Prueba1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using DevExpress.XamarinForms.DataForm;
+
+namespace Prueba1
+{
+    public class PersonalInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public virtual Gender Gender { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+    public enum Gender { Female, Male, RatherNotSay }
+
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+            DevExpress.XamarinForms.DataForm.Initializer.Init();
+            dataForm.DataObject = new PersonalInfo();
+
+        }
+
+    }
+}
